a4175aa324bce397c83f130fb57a625bfd26474e
Bhagya Sri <bhagimundru@gmail.com>
2022-12-22 16:46:48 +0530

    Identify code

--- script.js
+++ script.js
@@ -1,52 +1,69 @@
 
-  (function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
-      (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
-    var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
-    var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
-  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-JKAKZLOR3QV4-2");
+(function (n, t, a, e, co) {
+    var i = "aptrinsic"; n[i] = n[i] || function () {
+        (n[i].q = n[i].q || []).push(arguments)
+    }, n[i].p = e; n[i].c = co;
+    var r = t.createElement("script"); r.async = !0, r.src = a + "?a=" + e;
+    var c = t.getElementsByTagName("script")[0]; c.parentNode.insertBefore(r, c)
+})(window, document, "https://web-sdk.aptrinsic.com/api/aptrinsic.js", "AP-JKAKZLOR3QV4-2");
 
 
 
 function allowuser(user) {
-    let a= document.getElementById("user").value;
-    if (a=="bhagimundru@gmail.com" || a=="demouser@gmail.com"){
+    let a = document.getElementById("user").value;
+    var b = a.substr(8, 5);
+            var id = b;
+    if (a == "bhagimundru@gmail.com" || a == "demouser@gmail.com") {
         alert("Valid User");
+        //passing user and account objects:
+        aptrinsic("identify",
+            {
+                //User Fields
+                "id": id, // Required for logged in app users
+                "email": a
+            
+            },
+            {
+                //Account Fields
+                "id": "GSID-1234", //Required
+                "name": "International Business Machine"
+            });
         window.location = "https://bhagimundru.github.io/Basic_html/Home.html";
         alert("Valid User");
     }
-    else{
+    else {
         alert("Please enter a valid username");
     }
-    }
+}
 
 // document.getElementById("buttonOne").onclick=function(){
 //     ClickbuttonOne()
 // };
-function ClickbuttonOne(){
+function ClickbuttonOne() {
     //alert("hello from button one");
-    document.getElementById("extrainfoOne").style.display="block";
-    document.getElementById("buttonOne").style.display="none";
-}2
+    document.getElementById("extrainfoOne").style.display = "block";
+    document.getElementById("buttonOne").style.display = "none";
+} 2
 
-function SubcategoryDropdown(){
-    if( document.getElementById("Issue category").value=="JO"){
-    document.getElementById("JO").style.display="block";
-    document.getElementById("Rules").style.display="none";
-    document.getElementById("Cockpit").style.display="none";
+function SubcategoryDropdown() {
+    if (document.getElementById("Issue category").value == "JO") {
+        document.getElementById("JO").style.display = "block";
+        document.getElementById("Rules").style.display = "none";
+        document.getElementById("Cockpit").style.display = "none";
     }
-    if( document.getElementById("Issue category").value=="Rules"){
-        document.getElementById("JO").style.display="none";
-        document.getElementById("Rules").style.display="block";
-        document.getElementById("Cockpit").style.display="none";
+    if (document.getElementById("Issue category").value == "Rules") {
+        document.getElementById("JO").style.display = "none";
+        document.getElementById("Rules").style.display = "block";
+        document.getElementById("Cockpit").style.display = "none";
     }
-    if( document.getElementById("Issue category").value=="Cockpit"){
-    document.getElementById("JO").style.display="none";
-    document.getElementById("Rules").style.display="none";
-    document.getElementById("Cockpit").style.display="block";
+    if (document.getElementById("Issue category").value == "Cockpit") {
+        document.getElementById("JO").style.display = "none";
+        document.getElementById("Rules").style.display = "none";
+        document.getElementById("Cockpit").style.display = "block";
     }
-    
+
 }
 
 function ButtonDropdown() {
     document.getElementById("Dropdown").classList.toggle("show");
-  }
+}
