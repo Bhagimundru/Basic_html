b90a86258a5076b796091dfca23e816fda4ebd21
Bhagya Sri <bhagimundru@gmail.com>
2023-01-04 09:06:26 +0530

    Script change

--- script.js
+++ script.js
@@ -35,6 +35,7 @@ function allowuser(user) {
             alert("Logged in user id :"+b);
             //window.location = "https://bhagimundru.github.io/Basic_html/Home.html";
             window.location = "Home.html";
+            aptrinsic('track', 'User Login');
             alert("Logged in user id :"+b);
         
     }
