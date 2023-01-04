4571ebee01c913f09ebdf9ace829e98994ac11be
Bhagya Sri <bhagimundru@gmail.com>
2023-01-04 16:04:40 +0530

    Script Change

---
+++ scripttest.js
@@ -0,0 +1,70 @@
+
+(function (n, t, a, e, co) {
+    var i = "aptrinsic"; n[i] = n[i] || function () {
+        (n[i].q = n[i].q || []).push(arguments)
+    }, n[i].p = e; n[i].c = co;
+    var r = t.createElement("script"); r.async = !0, r.src = a + "?a=" + e;
+    var c = t.getElementsByTagName("script")[0]; c.parentNode.insertBefore(r, c)
+})(window, document, "https://web-sdk.aptrinsic.com/api/aptrinsic.js", "AP-JKAKZLOR3QV4-2");
+
+
+
+function allowuser(user) {
+    let a = document.getElementById("user").value;
+    var b;
+    if (a == "bhagimundru@gmail.com" || a == "demouser@gmail.com") {
+        alert("Valid User");
+        b= a.substr(8,5);
+        var id = b;
+
+        //passing user and account objects:
+        aptrinsic("identify",
+            {
+                //User Fields
+                "id": id, // Required for logged in app users
+                "email": a,
+                "userrole": "Admin"
+            
+            },
+            {
+                //Account Fields
+                "id": "GSID-1234", //Required
+                "name": "International Business Machine"
+            });
+
+            alert("Logged in user id :"+b);
+            //window.location = "https://bhagimundru.github.io/Basic_html/Home.html";
+            window.location = "Index.html";
+            //aptrinsic('track', 'User Login');
+            alert("Logged in user id :"+b);
+        
+    }
+    else {
+        alert("Please enter a valid username");
+    }
+    return b;
+}
+function ClickbuttonOne() {
+    //alert("hello from button one");
+    document.getElementById("extrainfoOne").style.display = "block";
+    document.getElementById("buttonOne").style.display = "none";
+}
+
+function SubcategoryDropdown() {
+    if (document.getElementById("Issue category").value == "JO") {
+        document.getElementById("JO").style.display = "block";
+        document.getElementById("Rules").style.display = "none";
+        document.getElementById("Cockpit").style.display = "none";
+    }
+    if (document.getElementById("Issue category").value == "Rules") {
+        document.getElementById("JO").style.display = "none";
+        document.getElementById("Rules").style.display = "block";
+        document.getElementById("Cockpit").style.display = "none";
+    }
+    if (document.getElementById("Issue category").value == "Cockpit") {
+        document.getElementById("JO").style.display = "none";
+        document.getElementById("Rules").style.display = "none";
+        document.getElementById("Cockpit").style.display = "block";
+    }
+
+}
