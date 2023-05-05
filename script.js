7bf4358eba2807598d03bcb39bd4cfca5ff8a36b
Bhagya Sri <bhagimundru@gmail.com>
2023-05-05 19:16:39 +0530

    Global Context

--- script.js
+++ script.js
@@ -16,7 +16,8 @@
   analytics.load("Hfg1fBe2BwxFi7g8YLQDInaSJHa2yrxS");
   analytics.page();
   }}();*/
-
+/* Full story Code*/
+/*
 window['_fs_host'] = 'fullstory.com';
 window['_fs_script'] = 'edge.fullstory.com/s/fs.js';
 window['_fs_org'] = 'o-1KN9BE-na1';
@@ -37,7 +38,7 @@ window['_fs_namespace'] = 'FS';
     g._w={};y='XMLHttpRequest';g._w[y]=m[y];y='fetch';g._w[y]=m[y];
     if(m[y])m[y]=function(){return g._w[y].apply(this,arguments)};
     g._v="1.3.0";
-})(window,document,window['_fs_namespace'],'script','user');
+})(window,document,window['_fs_namespace'],'script','user');*/
 
 
 function allowuser(form) {
