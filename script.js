124d2c3529e63c751df2a9542877a167fc9d7316
Bhagya Sri <bhagimundru@gmail.com>
2023-12-12 00:54:17 +0530

    errorhandling

--- script.js
+++ script.js
@@ -27,9 +27,6 @@ window.onerror = function(message, source, lineno, colno, error) {
     console.log(errorObject);
     aptrinsic('track', 'consoleError', {"name":errorObject.message});
 }
-    // Prevent the default browser error handling
-    return true;
-};
 
 /*!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="Hfg1fBe2BwxFi7g8YLQDInaSJHa2yrxS";;analytics.SNIPPET_VERSION="4.15.3";
   analytics.load("Hfg1fBe2BwxFi7g8YLQDInaSJHa2yrxS");
