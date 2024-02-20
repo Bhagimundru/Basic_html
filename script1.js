bac695070ac1caece086ff404244bd9c96de2b45
Bhagya Sri <bhagimundru@gmail.com>
2024-02-20 23:19:54 +0530

    stageANDchecksum

--- script1.js
+++ script1.js
@@ -3,7 +3,9 @@
     (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
   var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
   var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
-})(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-MTUFRPVCOSHV-2-2");
+})(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-MTUFRPVCOSHV-2-2",{
+    "engagementChecksumFileUrl": "Engagement_checksum_Onestop_Stage.json"
+});
 
 
 function CallStageIdentify(){
