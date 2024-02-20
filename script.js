3ff7a3d213ee8b4d396fbd52222ff01d4a02d182
Bhagya Sri <bhagimundru@gmail.com>
2024-02-20 12:10:55 +0530

    rudderstack

--- script.js
+++ script.js
@@ -7,11 +7,31 @@
     var c = t.getElementsByTagName("script")[0]; c.parentNode.insertBefore(r, c)
 })(window, document, "https://web-sdk.aptrinsic.com/api/aptrinsic.js", "AP-JKAKZLOR3QV4-2");*/
 
-(function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
+/*(function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
       (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
     var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
     var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
-  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-MTUFRPVCOSHV-2",{ "iframeModeEnabled": false });
+  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-MTUFRPVCOSHV-2",{ "iframeModeEnabled": false });*/
+
+
+
+  !function(){"use strict";var sdkBaseUrl="https://cdn.rudderlabs.com/v3";var sdkName="rsa.min.js";var asyncScript=true
+  ;window.rudderAnalyticsBuildType="legacy",window.rudderanalytics=[]
+  ;var e=["setDefaultInstanceKey","load","ready","page","track","identify","alias","group","reset","setAnonymousId","startSession","endSession"]
+  ;for(var t=0;t<e.length;t++){var n=e[t];window.rudderanalytics[n]=function(e){return function(){
+  window.rudderanalytics.push([e].concat(Array.prototype.slice.call(arguments)))}}(n)}try{
+  new Function('return import("")'),window.rudderAnalyticsBuildType="modern"}catch(a){}
+  if(window.rudderAnalyticsMount=function(){
+  "undefined"==typeof globalThis&&(Object.defineProperty(Object.prototype,"__globalThis_magic__",{get:function get(){
+  return this},configurable:true}),__globalThis_magic__.globalThis=__globalThis_magic__,
+  delete Object.prototype.__globalThis_magic__);var e=document.createElement("script")
+  ;e.src="".concat(sdkBaseUrl,"/").concat(window.rudderAnalyticsBuildType,"/").concat(sdkName),e.async=asyncScript,
+  document.head?document.head.appendChild(e):document.body.appendChild(e)
+  },"undefined"==typeof Promise||"undefined"==typeof globalThis){var d=document.createElement("script")
+  ;d.src="https://polyfill.io/v3/polyfill.min.js?features=Symbol%2CPromise&callback=rudderAnalyticsMount",
+  d.async=asyncScript,document.head?document.head.appendChild(d):document.body.appendChild(d)}else{
+  window.rudderAnalyticsMount()}window.rudderanalytics.load("2ccVMmO8Xszt55NFFD0Bx8fNBXb","https://gainsightagfyq.dataplane.rudderstack.com",{})}();    
+
 
 window.onerror = function(message, source, lineno, colno, error) {
     // Construct an error object
